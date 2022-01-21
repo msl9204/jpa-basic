884f5935b1fba3a5f38012239052658f7bee0f89
minsoo.lee <letomok@gmail.com>
2022-01-21 22:40:09 +0900

    즉시로딩, 지연로딩

--- src/main/java/jpabasic/ex1hellojpa/JpaBasicApplication.java
+++ src/main/java/jpabasic/ex1hellojpa/JpaBasicApplication.java
@@ -19,21 +19,30 @@ public class JpaBasicApplication {
 
         try {
 
+            Team team = new Team();
+            team.setName("teamA");
+            em.persist(team);
+
+            Team teamB = new Team();
+            teamB.setName("teamB");
+            em.persist(teamB);
 
             Member member1 = new Member();
             member1.setUsername("member1");
+            member1.setTeam(team);
             em.persist(member1);
+
+            Member member2 = new Member();
+            member2.setUsername("member2");
+            member2.setTeam(teamB);
+            em.persist(member2);
 
             em.flush();
             em.clear();
 
-            Member refMember = em.getReference(Member.class, member1.getId());
-            System.out.println("refMember = " + refMember.getClass().getName());
+            Member m = em.find(Member.class, member1.getId());
 
-            Hibernate.initialize(refMember); // 강제 초기화
-
-
-            System.out.println("isLoaded = " + emf.getPersistenceUnitUtil().isLoaded(refMember));
+            List<Member> members = em.createQuery("select m from Member m join fetch m.team", Member.class).getResultList();
 
             tx.commit();
             System.out.println("======================================2");
