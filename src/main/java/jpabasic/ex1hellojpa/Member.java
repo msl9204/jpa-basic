4d43a0bc6f5eb5e8edb64f57954235b155f64c4e
minsoo.lee <letomok@gmail.com>
2022-01-23 17:04:34 +0900

    임베디드 타입

--- src/main/java/jpabasic/ex1hellojpa/Member.java
+++ src/main/java/jpabasic/ex1hellojpa/Member.java
@@ -7,7 +7,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Entity
-public class Member extends BaseEntity {
+public class Member {
 
     @Id @GeneratedValue
     @Column(name = "MEMBER_ID")
@@ -16,9 +16,25 @@ public class Member extends BaseEntity {
     @Column(name = "USERNAME")
     private String username;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "TEAM_ID")
-    private Team team;
+    // 기간 Period
+    @Embedded
+    private Period workPeriod;
+
+    // 주소
+    @Embedded
+    private Address homeAddress;
+
+    // 임베디드 타입 중복으로 쓸 경우 재정의 필요
+    @Embedded
+    @AttributeOverrides({
+            @AttributeOverride(name = "city",
+                            column = @Column(name = "WORK_CITY")),
+            @AttributeOverride(name = "street",
+                            column = @Column(name = "WORK_STREET")),
+            @AttributeOverride(name = "zipcode",
+                            column = @Column(name = "WORK_ZIPCODE"))
+    })
+    private Address workAddress;
 
     public Long getId() {
         return id;
@@ -36,11 +52,19 @@ public class Member extends BaseEntity {
         this.username = username;
     }
 
-    public Team getTeam() {
-        return team;
+    public Period getWorkPeriod() {
+        return workPeriod;
     }
 
-    public void setTeam(Team team) {
-        this.team = team;
+    public void setWorkPeriod(Period workPeriod) {
+        this.workPeriod = workPeriod;
+    }
+
+    public Address getHomeAddress() {
+        return homeAddress;
+    }
+
+    public void setHomeAddress(Address homeAddress) {
+        this.homeAddress = homeAddress;
     }
 }
